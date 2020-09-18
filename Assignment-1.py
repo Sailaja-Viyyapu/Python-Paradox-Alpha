f5c063dca4b1cd43f7c2044c51e969c1e6c1176b
Sailaja-Viyyapu <68277518+Sailaja-Viyyapu@users.noreply.github.com>
2020-09-18 16:46:44 +0530

    Create Assignment-1.py

---
+++ Assignment-1.py
@@ -0,0 +1,41 @@
+1. Distinguish between variable, constant and literals with example.
+
+ANSWER:
+--------
+variable :- It is a name of location in memory that enables us to store a value for later use. 
+            It is called a variable because you can change the content in it whenever you want.
+example :- i=10 // here 'i' is a variable
+           i="python world"
+
+constant :- It is also like a variable.
+            But the only difference is that, it cannot be changed once defined.
+ example :- const i=10 // here 'i' is a constant, it value cannot be changed throughout the execution of the programme.
+ 
+ literal :- Literals are values assigned to variables and constants.
+ example :- i=10 // here 10 is literal.
+
+
+2.Define identifiers and datatypes.
+
+ANSWER:
+---------
+identifiers :-An identifier is a name used to identify a variable, function, class, module or other object. 
+              An identifier starts with a letter "A to Z" or "a to z" or an underscore (_) followed by zero or more letters, underscores and digits (0 to 9).
+example :- name ="sailaja viyyapu".
+
+datatypes :-Data types are the classification or categorization of data items. 
+           Data types represent a kind of value which determines what operations can be performed on that data. 
+           Numeric, non-numeric and Boolean (true/false) data are the most used data types.
+ Examples :-Integer, float, boolean,list,tuple... etc
+
+3.Write a program to declare two variable, take one of them from user and determine one by self then compare and print them.
+
+CODE:
+-----
+    
+    var1=input("Enter a value:")
+    print(var1)
+    var2=20
+    print(var1==var2)
+    
+    
