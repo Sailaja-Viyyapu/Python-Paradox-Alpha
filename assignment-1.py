58fdd0c9bf60368cdafa0f9d57f292c07081f373
Sailaja-Viyyapu <68277518+Sailaja-Viyyapu@users.noreply.github.com>
2020-10-06 10:52:03 +0530

    Create assignment-1.py

---
+++ assignment-1.py
@@ -0,0 +1,48 @@
+                                        ASSIGNMENT-1
+                                                                    Name :- Viyyapu Sailaja
+                                                               Mobile no :- 7997688100
+                                                               
+ ------------ANSWERS----------
+ 
+ 1)
+num1=int(input("Enter A no:"))
+num2=int(input("Enter A no:"))
+if(num1!=num2):
+    if(num1>num2):
+        print("smaller number is : ",num2)
+        print("Greater number is :",num1)
+    else:
+        print("smaller number is : ",num1)
+        print("Greater number is :",num2)
+else:
+    print("both numbers are equal",num1)
+    
+ 2)
+paragraph=str(input("Enter some text :"))
+print(paragraph.title())
+
+3)
+paragraph1="there is company named is. it is constructed in island."
+print(paragraph1.find("is"))
+print(paragraph1.count("is"))
+
+5)----------standard data types-------
+
+Data types are the classification or categorization of data items.
+Data types represent a kind of value which determines what operations can be performed on that data
+1.	Integer : we use it for number postive or negative but without decimal point 
+        int a=10 or int a=-10
+2.	Character : we use it for only single alphabet
+        char c='s'
+3.	String : we use it for name and any collect of alphabet
+        Str s="fruits"
+4.	Float : we use it for number in decimal
+        Float f=10.333
+5.	Complex : A number with a real and imaginary component represented as x+yj. 
+                x and y are floats and j is -1(square root of -1 called an imaginary number)
+        Complex cp=5i+4j
+6.	Double #we use it for number and char Inc. the size 
+7.	Long #we use it for number and char also it creates whole block and also Inc. the size
+8.	Short #we use it for number
+9.	Unsigned #we use it for number
+10.	Signed, etc #we use it for number
